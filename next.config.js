eed77f6287983327bfeed888b9fe566e25c04aba
Paula I <illanesrpaula@gmail.com>
2024-10-03 01:04:07 -0300

    fix: changing new url for strapi images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["127.0.0.1", "strapi-ecommerce-production-85a7.up.railway.app", "localhost:1336"]
+    domains: ["127.0.0.1", "strapi-ecommerce-production-25bc.up.railway.app", "localhost:1336"]
   },
   env: {
     NEXT_PUBLIC_STRAPI_URL: process.env.NEXT_PUBLIC_STRAPI_URL
